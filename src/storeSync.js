93b597bfdf8507dbc318c16d919c5dc40c0442e4
Sampo Juustila <sampo.juustila@iki.fi>
2019-02-20 21:48:20 +0200

    feat: Add backend data syncing

---
+++ src/storeSync.js
@@ -0,0 +1,40 @@
+import store from './store'
+import io from 'socket.io-client'
+import axios from 'axios'
+
+export function fetchDataBlobs(path) {
+  axios.get(path, { baseURL: store.state.backend.uri })
+  .then(response => {
+    if (Array.isArray(response.data)) {
+      response.data.forEach(e => store.commit('setDataBlob', e))
+    } else {
+      store.commit('setDataBlob', response.data)
+    }
+  })
+  .catch(e => {
+    console.error("Error fetching data blobs (retrying in 5 secs): ", e)
+    setTimeout(() => fetchDataBlobs(path), 5000)
+  })
+}
+
+/*
+ * Usage example:
+ *   startDataBlobSync('/box')        -  sync all 'box' types
+ *   startDataBlobSync('/box/myid')   -  sync single data blob
+ */
+export function startDataBlobSync(path) {
+  path = `/data${path || ''}`
+  console.log("Initializing data blob syncing from " + store.state.backend.uri + " with path " + path)
+
+  fetchDataBlobs(path)
+
+  const socket = io(`${store.state.backend.uri}/data?data=${path}`, {})
+  socket.on('dataUpdate', (type, id, value) => {
+    console.log('dataUpdate: ' + type + ' ' + id, value)
+    store.commit('setDataBlob', value)
+  });
+  socket.on('dataDelete', (type, id) => {
+    console.log('dataDelete: ' + type + ' ' + id)
+    store.commit('deleteDataBlob', {type, id})
+  });
+}
--- src/views/HangarBayDoor.vue
+++ src/views/HangarBayDoor.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Pressure:</h1>
+    <h2>{{box.pressure}}</h2>
   </div>
 </template>
+
+<script>
+import { startDataBlobSync } from '../storeSync'
+
+const DEFAULT_BOX = {}
+
+export default {
+  computed: {
+    box () {
+      return this.$store.state.dataBlobs.find(e => e.type === 'box' && e.id === this.$store.state.boxId) || DEFAULT_BOX
+    }
+  },
+  created () {
+    startDataBlobSync(`/box/${this.$store.state.boxId}`)
+  },
+}
+</script>
